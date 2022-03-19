021f859941157962668012173e44f0c55707faba
Maina Wycliffe <wmmaina7@gmail.com>
2022-03-19 11:26:54 +0300

    Setup Tailwind CSS

--- app/root.tsx
+++ app/root.tsx
@@ -1,22 +1,20 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "remix";
-import type { MetaFunction } from "remix";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from 'remix';
+import type { MetaFunction } from 'remix';
+import styles from './tailwind.css';
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
 });
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
