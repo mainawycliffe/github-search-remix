2b2d52e138eadc64020b5bbe3f9fb80841f3d89b
Maina Wycliffe <wmmaina7@gmail.com>
2022-03-19 11:43:42 +0300

    Implement Search for on the Landing Page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,24 +1,21 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a className='font-semibold' target='_blank' href='https://remix.run/tutorials/blog' rel='noreferrer'>
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/tutorials/jokes' rel='noreferrer'>
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className='flex flex-col justify-center items-center h-screen w-screen bg-gray-300'>
+      <div className='flex flex-col space-y-4'>
+        <form method='get' action='/search' className='space-y-4'>
+          <label className='font-bold text-2xl' htmlFor='search'>
+            Search on Github
+          </label>
+          <input
+            type='text'
+            id='search'
+            name='search'
+            className='bg-gray-200 appearance-none border-2 border-gray-600 rounded-md w-full py-2 px-4 text-gray-700 leading-tight focus:bg-white focus:border-blue-300 text-xl'
+            placeholder='Search'
+          />
+          <button className='text-xl px-4 py-2 text-white bg-blue-500 rounded-lg font-bold'>Search</button>
+        </form>
+      </div>
     </div>
   );
 }
