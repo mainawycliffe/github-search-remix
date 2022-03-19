9346d7d33ecac04b13f4100d1a1eb9018284a954
Maina Wycliffe <wmmaina7@gmail.com>
2022-03-20 01:01:03 +0300

    Rename the Page Title to Github Search App

--- app/root.tsx
+++ app/root.tsx
@@ -4,7 +4,7 @@ import styles from './tailwind.css';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Github Search App',
   viewport: 'width=device-width,initial-scale=1',
 });
 
